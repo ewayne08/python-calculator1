4e33052724f5a04e7ac4aa34f761fcc0a738fb20
Ethan <ethanchristianwayne@gmail.com>
2021-09-18 16:54:09 -0400

    Complete

--- calculator.py
+++ calculator.py
@@ -12,6 +12,69 @@ print("""
 8. Calculate the area of square
 9. Finding the sqaure root
 
+For exit press 0
+
 
 
 """)
+
+Operation = int(input("Please choose operation you want to do: "))
+
+if Operation == 1:
+    Number_1 = int(input("Please enter the first number: "))
+    Number_2 = int(input("Please enter the second number: "))
+
+    print("Result: ", Number_1 + Number_2)
+
+elif Operation == 2:
+     Number_1 = int(input("Please enter the first number: "))
+     Number_2 = int(input("Please enter the second number: "))
+
+     print("Result: ", Number_1 - Number_2 )
+
+elif Operation == 3: 
+     Number_1 = int(input("Please enter the first number: "))
+     Number_2 = int(input("Please enter the second number: "))
+
+     print("Result: ", Number_1*Number_2)
+
+elif Operation == 4:
+     Number_1 = int(input("Please enter the first number: "))
+     Number_2 = int(input("Please enter the second number: "))
+
+     print("Result ", Number_1/Number_2 )
+
+elif Operation == 5:
+    Number_1 = int(input("Please enter a number: "))
+
+    print("Result: ", Number_1**2)
+
+elif Operation == 6:
+    Weight = float(input("Please enter your weight: "))
+    Height = float(input("Please enter your height: "))
+
+    print("BMI: ", Weight / (Height ** 2))
+
+elif Operation == 7:
+
+    Base = int(input("Please enter the base: "))
+    Height = int(input("Please enter the height: "))
+
+    print("Result: ", (Base * Height)/2)
+
+elif Operation == 8:
+
+    Side = int(input("Please enter the side: "))
+
+    print("Result: ", Side**2)
+
+elif Operation == 9:
+    Number = int(input("Please enter a number: "))
+
+    print("Result: ", Number ** 0.5)
+
+elif Operation == 0:
+    print("Program is shutting down...")
+
+else:
+    print("Invalid Operation")
