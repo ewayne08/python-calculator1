e552fce7eb29ba0bb3c65d342f96c78799b7a360
Ethan <ethanchristianwayne@gmail.com>
2021-09-18 15:41:57 -0400

    Start of Calculator

---
+++ calculator.py
@@ -0,0 +1,17 @@
+import math
+
+print("""
+
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+5. Exponent
+6. Calculate BMI
+7. Calculate the area of triangle
+8. Calculate the area of square
+9. Finding the sqaure root
+
+
+
+""")
